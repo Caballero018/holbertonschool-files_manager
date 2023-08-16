7056b2036777d82b12148d4333290c1c6c60b5c2
Caballero018 <alejoxcaballero18@gmail.com>
2023-08-15 20:41:06 -0500

    feature: add Crud for client redis

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/prefer-default-export */
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error('Error en el cliente Redis:', error);
+    });
+  }
+
+  isAlive() {
+    if (this.client.connected) return true;
+
+    return false;
+  }
+
+  async get(key) {
+    this.client.get(key, (error, value) => value);
+  }
+
+  async set(key, value, seconds) {
+    this.client.setex(key, seconds, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+export const redisClient = new RedisClient();
